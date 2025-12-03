831251912bb4aa641814e279b25c6152db426f40
Mikolaj Bilski <mj.bilski@student.uw.edu.pl>
2025-12-03 18:29:10 +0100

    feat: use new specials system

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-﻿function getRandomArbitrary(min, max) {
+﻿import * as Special from "./special.js"
+
+function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
 
@@ -143,81 +145,16 @@ function losowanko() {
     }
     else {
         progress.innerHTML = "losowanko zakonczone, jeszcze raz?";
-        if (b2.innerHTML == w1[7] && b3.innerHTML == w2[21]) {
-            a4.src = "sounds/tmsll.mp3"
+        var losowanko_res = new Special.Losowanko(b1.innerHTML, b2.innerHTML, b3.innerHTML)
+        var special = Special.get_special(losowanko_res)
+        if (special) {
+            a4.src = "sounds/" + special.audio_name + ".mp3"
             a4.play();
 
-            progress.innerHTML = "specjal napędził: Todd Howard <br />oryginał: <a href='https://www.youtube.com/watch?v=yvGXCisAaR4'>Link do YT</a><br />losowanko zakonczone, jeszcze raz?";
-            specjalne += 1;
-        }
-        else if (b2.innerHTML == w1[25] && b3.innerHTML == w2[35]) {
-            a4.src = "sounds/roltest.mp3"
-            a4.play();
-            progress.innerHTML = "specjal napędził: Klejnot Nilu & Testoviron <br />oryginał: <a href='https://www.youtube.com/watch?v=4kbpssHwqYc'>Link do YT [+18]</a><br />losowanko zakonczone, jeszcze raz?";
-            specjalne += 1;
-        }
-        else if (b1.innerHTML == 5 && b2.innerHTML == w1[0] && b3.innerHTML == w2[0]) {
-            a4.src = "sounds/cytryny.mp3"
-            a4.play();
-            progress.innerHTML = "specjal napędził: Klo Cuch <br />oryginał: <a href='https://www.youtube.com/watch?v=H6qsJcALHkM'>Link do YT</a><br />losowanko zakonczone, jeszcze raz?";
-            specjalne += 1;
-        }
-        else if (b2.innerHTML == w1[36]) {
-            ryjceDiv.innerHTML = "temacik dla: <img height='100px' class='shadowed' src='img/szef.png' />";
-            a4.src = "sounds/tonasiebie.mp3"
-            a4.play();
-            progress.innerHTML = "specjal napędził: Klo Cuch <br />oryginał: <a href='https://www.youtube.com/watch?v=H6qsJcALHkM'>Link do YT</a><br />losowanko zakonczone, jeszcze raz?";
-            specjalne += 1;
-        }
-        else if (b2.innerHTML == w1[1]) {
-            ryjceDiv.innerHTML = "temacik dla: <img height='100px' class='shadowed' src='img/jordan.png' />";
-            a4.src = "sounds/chinskie.mp3"
-            a4.play();
-            progress.innerHTML = "specjal napędził: Klo Cuch <br />oryginał: <a href='https://www.youtube.com/watch?v=H6qsJcALHkM'>Link do YT</a><br />losowanko zakonczone, jeszcze raz?";
-            specjalne += 1;
-        }
-        else if (b2.innerHTML == "wiedzmin 3") {
-            b3.innerHTML = "najlepszy"
-            a4.src = "sounds/3w3.mp3"
-            a4.play();
-            progress.innerHTML = "specjal napędził: Klo Cuch <br />oryginał: <a href='https://www.youtube.com/watch?v=H6qsJcALHkM'>Link do YT</a><br />losowanko zakonczone, jeszcze raz?";
-            specjalne += 1;
-        }
-        else if (b2.innerHTML == w1[40] && b3.innerHTML == w2[68]) {
-            a4.src = "sounds/to.mp3"
-            a4.play();
-            progress.innerHTML = "specjal napędził: Husson Cuch <br />oryginał: <a href='https://www.youtube.com/watch?v=sS3xkUyqyUQ'>Link do YT</a><br />losowanko zakonczone, jeszcze raz?";
-            specjalne += 1;
-        }
-        else if (b2.innerHTML == w1[42] && b3.innerHTML == w2[100]) {
-            ryjceDiv.innerHTML = "temacik dla: <img height='100px' class='shadowed' src='img/kacper.png />";
-            a4.src = "sounds/kruci.mp3"
-            a4.play();
-            progress.innerHTML = "specjal napędził: Klo Cuch <br />oryginał: <a href='https://www.youtube.com/watch?v=sS3xkUyqyUQ'>Link do YT</a><br />losowanko zakonczone, jeszcze raz?";
-            specjalne += 1;
-        }
-		else if (b2.innerHTML == w1[59] && b3.innerHTML == w2[104]) {
-            a4.src = "sounds/jg.mp3"
-            a4.play();
-            progress.innerHTML = "specjal napędził: Klo Cuch <br />oryginał: <a href='https://www.youtube.com/watch?v=lwJMwKgejng'>Link do YT</a><br />losowanko zakonczone, jeszcze raz?";
-            specjalne += 1;
-        }
-		else if (b2.innerHTML == w1[58] && b3.innerHTML == w2[105]) {
-            a4.src = "sounds/natsp.mp3"
-            a4.play();
-            progress.innerHTML = "specjal napędził: Klo Cuch <br />oryginał: <a href='https://www.youtube.com/watch?v=lwJMwKgejng'>Link do YT</a><br />losowanko zakonczone, jeszcze raz?";
-            specjalne += 1;
-        }
-		else if (b3.innerHTML == w2[112]) {
-            a4.src = "sounds/cwiercz.mp3"
-            a4.play();
-            progress.innerHTML = "specjal napędził: Klo Cuch <br />oryginał: <a href='https://www.youtube.com/watch?v=rxlqV0f4us0'>Link do YT</a><br />losowanko zakonczone, jeszcze raz?";
-            specjalne += 1;
-        }
-		else if (b3.innerHTML == w2[92]) {
-            a4.src = "sounds/gin.mp3"
-            a4.play();
-            progress.innerHTML = "specjal napędził: Kistru ft Kaktus & Gothic <br />oryginał: <a href='https://www.youtube.com/watch?v=y_ifjEYEMuY'>Link do YT</a><br />losowanko zakonczone, jeszcze raz?";
+            if (special.author != '*') {
+                ryjceDiv.innerHTML = "temacik dla: <img height='100px' class='shadowed' src='img/" + special.author + ".png' />";
+            }
+
             specjalne += 1;
         }
         else {
