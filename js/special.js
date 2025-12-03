f3ebf7354c123eceeead1ea0d841b6909c3dcdab
Mikolaj Bilski <mj.bilski@student.uw.edu.pl>
2025-12-03 18:07:36 +0100

    feat: add Special class

---
+++ js/special.js
@@ -0,0 +1,21 @@
+export class Special {
+    // use "*" for wildcard (if a special doesn't depend on the specific field of losowanko)
+    constructor(num, t1, t2, audio_name, author) {
+        this.num = num
+        this.t1 = t1
+        this.t2 = t2
+        this.audio_name = audio_name
+        this.author = author
+    }
+}
+
+var specials = [
+    new Special("*", "jednowątkowych", "*", "jaki-jestes-wiciu", "wiciu"),
+    new Special("*", "*", "wiader", "kaszelek", "wiciu"),
+]
+
+
+// returns the object representing the special temacik, containing .audio_src and .author (strings)
+function get_special(losowanko) {
+
+}
