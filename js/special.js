4b7baf9f85e758eb5b62d049d160e16bf51a6df3
Mikolaj Bilski <mj.bilski@student.uw.edu.pl>
2025-12-03 18:17:13 +0100

    feat: introduce Losowanko class and add special matching

--- js/special.js
+++ js/special.js
@@ -1,3 +1,11 @@
+export class Losowanko {
+    constructor(num, t1, t2) {
+        this.num = num
+        this.t1 = t1
+        this.t2 = t2
+    }
+}
+
 export class Special {
     // use "*" for wildcard (if a special doesn't depend on the specific field of losowanko)
     constructor(num, t1, t2, audio_name, author) {
@@ -6,6 +14,12 @@ export class Special {
         this.t2 = t2
         this.audio_name = audio_name
         this.author = author
+    }
+
+    check_match(losowanko) {
+        return (this.num == "*" || this.num == losowanko.num) &&
+               (this.t1 == "*" || this.num == losowanko.t1) &&
+               (this.t2 == "*" || this.num == losowanko.t2)
     }
 }
 
@@ -17,5 +31,11 @@ var specials = [
 
 // returns the object representing the special temacik, containing .audio_src and .author (strings)
 function get_special(losowanko) {
+    for (const special of specials) {
+        if (special.check_match(losowanko)) {
+            return special
+        }
+    }
 
+    return null
 }
